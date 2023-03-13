cec5a001c82afaa44bf6ccc9b3fc127e3d836fae
= <=>
2023-03-13 15:39:07 +0100

    set BE link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  urlRestaurants: 'http://127.0.0.1:8080/api/restaurants',
-  urlplates:'http://127.0.0.1:8080/api/plates',
+  urlRestaurants: 'http://www.foodjekdelivery.com:8080/api/restaurants',
+  urlplates:'http://www.foodjekdelivery.com:8080/api/plates',
   production: false
 };
 
